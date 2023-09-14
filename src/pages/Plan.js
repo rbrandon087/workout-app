fcf750ea05655f3522de6479b8cd1325936831dd
Chris Wehner <chrisw7128@gmail.com>
2023-09-13 21:13:53 -0700

    got the signup working but the login is buggy

--- src/pages/Plan.js
+++ src/pages/Plan.js
@@ -1,8 +1,12 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { createClient } from "@supabase/supabase-js";
 
-function Plan() {
+const supabase = createClient(
+  "https://zwcwryojtrkygrslhndl.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3Y3dyeW9qdHJreWdyc2xobmRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQyOTE1MzIsImV4cCI6MjAwOTg2NzUzMn0.6rfOo0VhEbNh6MN5gjEtmHOFo1jEg0C3-D_o34JfZjU"
+);
 
-}
+function Plan() {}
 
 export default Plan;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../Navbar";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  "https://zwcwryojtrkygrslhndl.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3Y3dyeW9qdHJreWdyc2xobmRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQyOTE1MzIsImV4cCI6MjAwOTg2NzUzMn0.6rfOo0VhEbNh6MN5gjEtmHOFo1jEg0C3-D_o34JfZjU"
+);
 
 function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [signUpError, setSignUpError] = useState("");
 
-  const handleSignUp = (e) => {
+  const handleSignUp = async e => {
     e.preventDefault();
 
     if (email === "" || password === "") {
@@ -15,11 +21,41 @@ function SignUp() {
       return;
     }
 
-    // Your signup logic here (Supabase or other authentication logic)
-
-    // If successful:
-    // window.alert("Signup successful. Check your email for the verification link.");
+    try {
+      await add_signup_to_db();
+      window.alert(
+        "Signup successful. Check your email for the verification link."
+      );
+      // You can also redirect the user to another page if needed.
+    } catch (error) {
+      console.error("Error during signup:", error.message);
+      // Handle any signup-related errors here.
+    }
   };
+
+  async function add_signup_to_db() {
+    try {
+      const { data, error } = await supabase
+        .from("Users")
+        .insert([
+          {
+            email: email,
+            password: password,
+          },
+        ])
+        .single();
+
+      if (error) {
+        console.error("Error adding user to database:", error.message);
+      } else {
+        console.log("User added to database:", data);
+        // You can perform additional actions here after adding the user
+        // For example, show a success message or redirect to another page.
+      }
+    } catch (error) {
+      console.error("Error adding user to database:", error.message);
+    }
+  }
 
   const loginContainerStyle = {
     display: "flex",
@@ -59,8 +95,6 @@ function SignUp() {
 
   return (
     <div>
-      
-
       <div style={loginContainerStyle}>
         <div style={loginFormStyle}>
           <h1 style={{ textAlign: "center", marginBottom: "20px" }}>Sign Up</h1>
@@ -73,7 +107,7 @@ function SignUp() {
                 id="emailInput"
                 value={email}
                 style={inputStyle}
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={e => setEmail(e.target.value)}
               />
             </div>
             <div style={{ marginBottom: "10px" }}>
@@ -84,7 +118,7 @@ function SignUp() {
                 id="passwordInput"
                 value={password}
                 style={inputStyle}
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={e => setPassword(e.target.value)}
               />
             </div>
             <button style={buttonStyle} type="submit">
